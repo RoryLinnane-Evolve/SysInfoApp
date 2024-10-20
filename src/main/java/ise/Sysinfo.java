bb5196dc1d898a20b10efef4a752d637bed06ed0
Mikey Fennelly <mikeyp.fennelly@gmail.com>
2024-10-21 00:44:53 +0100

    removed redundant code from Sysinfo.main(). Began SysinfoInteface

--- src/main/java/ise/Sysinfo.java
+++ src/main/java/ise/Sysinfo.java
@@ -10,32 +10,11 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 public class Sysinfo {
-    public static void main(String[] args ) throws IOException {
 
-        String firstArg;
-        try {
-            firstArg = args[0];
-        } catch (ArrayIndexOutOfBoundsException e) {
-            firstArg = null;
-        }
+}
 
-        if (firstArg == null) {
-            new CLIUsageError().noArgs();
-        } else {
-            switch (firstArg) {
-                case "cpu":
-                    VirtualFileInfo cpuinfo = new VirtualFileInfo("/proc/cpuinfo");
-                    break;
-                case "mem":
-                    VirtualFileInfo meminfo = new VirtualFileInfo("/proc/meminfo");
-                    break;
-                case "allprocs":
-                    ProcessInfo processInfo = new ProcessInfo();
-                    ArrayList<Integer> currentProcs = processInfo.getAllRunningPIDs();
-
-                ProcessInfo testProc = new ProcessInfo();
-                testProc.setProcessInfoFromPID(3159);
-            }
-        }
-    }
+interface SysinfoInterface {
+    ArrayList<Integer> getRunningPIDs();
+    ProcessInfo getProcessInfo(int pid);
+    SystemMemoryInfo getSystemMemoryInfo();
 }
