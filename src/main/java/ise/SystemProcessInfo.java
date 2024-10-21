376a285d991259f873f385f56d4a0aa431c5493e
Mikey Fennelly <mikeyp.fennelly@gmail.com>
2024-10-21 13:26:35 +0100

    Removed non-system scoped information from ProcessInfo class and renamed class to SystemProcessInfo.

---
+++ src/main/java/ise/SystemProcessInfo.java
@@ -0,0 +1,29 @@
+package ise;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+/**
+ * This class can create an object with information about currently running processes
+ * on the host machine, such as name, pid, ppid, state
+ * @author Mikey Fennelly
+ * */
+public class SystemProcessInfo {
+    public static ArrayList<Integer> getAllRunningPIDs() {
+        ArrayList<Integer> runningPIDs = new ArrayList<>();
+
+        File procDir = new File("/proc");
+        String[] procDirs = procDir.list();
+
+        if (procDirs != null) {
+            for (String dir : procDirs) {
+                if (dir.matches("\\d+")) {
+                    runningPIDs.add(Integer.parseInt(dir));
+                }
+            }
+        }
+
+        return runningPIDs;
+    }
+}
