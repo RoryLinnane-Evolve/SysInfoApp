63c16662949f0fae105c59f9c2cbbc636151ebe3
Mikey Fennelly <mikeyp.fennelly@gmail.com>
2024-10-21 19:49:06 +0100

    Remove useless methods in CPU class

--- src/main/java/ise/CPU.java
+++ src/main/java/ise/CPU.java
@@ -1,17 +1,17 @@
 package ise;
 
+import java.io.IOException;
+import java.util.HashMap;
+
 public class CPU {
     private int numCPUs;
 
-    public CPU() {
-        setCPUInfo();
+    public static void main(String[] args) throws IOException {
+        HashMap<String, String[]> procStatHMap = stat();
     }
 
-    private void setCPUInfo() {
-        this.numCPUs = Runtime.getRuntime().availableProcessors();
-    }
-
-    public int getNumCPUs() {
-        return numCPUs;
+    public static HashMap<String, String[]> stat() throws IOException {
+        VirtualFileInfo procstat = new VirtualFileInfo("/proc/stat");
+        return procstat.getGenericHashMap();
     }
 }
