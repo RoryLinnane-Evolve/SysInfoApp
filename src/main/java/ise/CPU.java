7453ebe47d235fc479c83a7ba1004206ba9cf87f
Mikey Fennelly <mikeyp.fennelly@gmail.com>
2024-10-24 02:01:38 +0100

    created ProcStatVF class to represent virtual file /proc/stat information

--- src/main/java/ise/CPU.java
+++ src/main/java/ise/CPU.java
@@ -1,7 +1,7 @@
 package ise;
 
 import java.io.IOException;
-import java.util.HashMap;
+import java.util.*;
 
 public class CPU {
     private int numCPUs;
@@ -14,4 +14,41 @@ public class CPU {
         VirtualFileInfo procstat = new VirtualFileInfo("/proc/stat");
         procstat.setHashtable();
     }
+}
+
+class ProcStatVF extends VirtualFile<List<Integer>, Map<String, List<Integer>>> {
+    public ProcStatVF(String fileLocation) throws IOException {
+        super(fileLocation);
+    }
+
+    private Map<String, List<Integer>> getKVPs(){
+        Map<String, List<Integer>> KVPs = new HashMap<String, List<Integer>>();
+
+        List<String> lines = this.getLines();
+
+        for (int i = 0; i < lines.size(); i++) {
+            String line = lines.get(i);
+            StringTokenizer tokens = new StringTokenizer(line, " ");
+
+            if (tokens.hasMoreTokens()) {
+                List<Integer> thisLineValues = new ArrayList<Integer>();
+                String thisLineKey = tokens.nextToken();
+
+                while (tokens.hasMoreTokens()) {
+                    String value = tokens.nextToken();
+                    if (!value.trim().equals("")) {
+                        thisLineValues.add(Integer.parseInt(value));
+                    }
+                }
+                KVPs.put(thisLineKey, thisLineValues);
+            }
+        }
+        return KVPs;
+    }
+
+    public void printToStdout() {
+
+    }
+
+    public void sendToMaster() {}
 }
