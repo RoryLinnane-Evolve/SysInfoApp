bae05fca033869fb284ff8534891c3beb670d1e7
Mikey Fennelly <mikeyp.fennelly@gmail.com>
2024-10-24 02:03:35 +0100

    removed stat() method from class CPU

--- src/main/java/ise/CPU.java
+++ src/main/java/ise/CPU.java
@@ -7,12 +7,6 @@ public class CPU {
     private int numCPUs;
 
     public static void main(String[] args) throws IOException {
-        stat();
-    }
-
-    private static void stat() throws IOException {
-        VirtualFileInfo procstat = new VirtualFileInfo("/proc/stat");
-        procstat.setHashtable();
     }
 }
 
@@ -46,9 +40,7 @@ class ProcStatVF extends VirtualFile<List<Integer>, Map<String, List<Integer>>> 
         return KVPs;
     }
 
-    public void printToStdout() {
-
-    }
+    public void printToStdout() {}
 
     public void sendToMaster() {}
 }
