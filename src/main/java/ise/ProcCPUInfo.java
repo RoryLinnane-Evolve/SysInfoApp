7ee770a33698977da3fc1f4507a88560aa903c62
Mikey Fennelly <mikeyp.fennelly@gmail.com>
2024-10-26 12:54:34 +0100

    convertToAptType working, but verbose

--- src/main/java/ise/ProcCPUInfo.java
+++ src/main/java/ise/ProcCPUInfo.java
@@ -45,7 +45,106 @@ public class ProcCPUInfo extends VirtualFile {
         String value = valueUnconverted.trim();
         Object valueReturn = value;
 
+//        System.out.println("case \"" + key + "\":");
+//        System.out.println("    break;");
 
+        switch (key) {
+            case "processor":
+                valueReturn = Integer.parseInt(value);
+                break;
+            case "vendor_id":
+                valueReturn = value.trim();
+                break;
+            case "cpu family":
+                valueReturn = Integer.parseInt(value);
+                break;
+            case "model":
+                valueReturn = Integer.parseInt(value);
+                break;
+            case "model name":
+                valueReturn = value.trim();
+                break;
+            case "stepping":
+                valueReturn = Integer.parseInt(value);
+                break;
+            case "microcode":
+                valueReturn = Long.decode(value);
+                break;
+            case "cpu MHz":
+                valueReturn = Double.parseDouble(value);
+                break;
+            case "cache size":
+                valueReturn = Integer.parseInt(value.replace(" KB", ""));
+                break;
+            case "physical id":
+                valueReturn = Integer.parseInt(value);
+                break;
+            case "siblings":
+                valueReturn = Integer.parseInt(value);
+                break;
+            case "core id":
+                valueReturn = Integer.parseInt(value);
+                break;
+            case "cpu cores":
+                valueReturn = Integer.parseInt(value);
+                break;
+            case "apicid":
+                valueReturn = Integer.parseInt(value);
+                break;
+            case "initial apicid":
+                valueReturn = Integer.parseInt(value);
+                break;
+            case "fpu":
+                if (value == "yes"){
+                    valueReturn = true;
+                } else {
+                    valueReturn = false;
+                }
+                break;
+            case "fpu_exception":
+                if (value == "yes"){
+                    valueReturn = true;
+                } else {
+                    valueReturn = false;
+                }
+                break;
+            case "cpuid level":
+                valueReturn = Integer.parseInt(value);
+                break;
+            case "wp":
+                if (value == "yes"){
+                    valueReturn = true;
+                } else {
+                    valueReturn = false;
+                }
+                break;
+            case "flags":
+                valueReturn = value.split(" ");
+                break;
+            case "vmx flags":
+                valueReturn = value.split(" ");
+                break;
+            case "bugs":
+                valueReturn = value.split(" ");
+                break;
+            case "bogomips":
+                valueReturn = Double.parseDouble(value);
+                break;
+            case "clflush size":
+                valueReturn = Integer.parseInt(value);
+                break;
+            case "cache_alignment":
+                valueReturn = Integer.parseInt(value);
+                break;
+            case "address sizes":
+                String[] splitLine = value.split(", ");
+                Integer bitsPhysical = Integer.parseInt(splitLine[0].replace(" bits physical", ""));
+                Integer bitsVirtual = Integer.parseInt(splitLine[1].replace(" bits virtual", ""));
+                valueReturn = new Hashtable<String, Integer>();
+                ((Hashtable<String, Integer>) valueReturn).put("bitsPhysical", bitsPhysical);
+                ((Hashtable<String, Integer>) valueReturn).put("bitsVirtual", bitsVirtual);
+                break;
+        }
 
         return valueReturn;
     }
