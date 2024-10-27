32e2cda7b2348a4e2eaec2caa3fd87f8c25c9b99
Mikey Fennelly <mikeyp.fennelly@gmail.com>
2024-10-27 19:15:39 +0000

    Added processProcState function

--- src/main/java/ise/Process.java
+++ src/main/java/ise/Process.java
@@ -9,6 +9,8 @@
 package ise;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 enum ProcState {
     RUN, SLEEP, UNINTERRUPTABLE_SLEEP, STOPPED, ZOMBIE
@@ -19,5 +21,31 @@ enum ProcState {
  * in bash
  * */
 public class Process {
+    private static final List<String> trimOnlyItemsList = Arrays.asList("Name", "Speculation_Store_Bypass", "SpeculationIndirectBranch", "Cpus_allowed");
+    private static final List<String> popThreeCharsReturnIntList = Arrays.asList("VmPeak", "VmSize", "VmLck", "VmPin", "VmHWM", "VmRSS", "RssAnon", "RssFile", "RssShmem", "VmData", "VmStk", "VmExe", "VmLib", "VmPTE", "VmSwap", "HugetlbPages");
+
+
+    private static ConversionOperation processProcState = (state -> {
+        ProcState stateToReturn = null;
+        switch (state) {
+            case "S (sleeping)":
+                stateToReturn = ProcState.SLEEP;
+                break;
+            case "R (running)":
+                stateToReturn = ProcState.RUN;
+                break;
+            case "T (stopped)":
+                stateToReturn = ProcState.STOPPED;
+                break;
+            case "Z (zombie)":
+                stateToReturn = ProcState.ZOMBIE;
+                break;
+            case "D (uninterruptable sleep)":
+                stateToReturn = ProcState.UNINTERRUPTABLE_SLEEP;
+                break;
+        }
+        return stateToReturn;
+    });
+
 
 }
