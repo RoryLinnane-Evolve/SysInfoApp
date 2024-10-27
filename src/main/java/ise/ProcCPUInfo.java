352eed29755a4d4ba68a00d8018f0a0a834a6c53
Mikey Fennelly <mikeyp.fennelly@gmail.com>
2024-10-27 17:43:40 +0000

    if else block removed from ProcCPUInfo.getProcCPUInfotTables() | Signature types exposing less  interface information to caller.

--- src/main/java/ise/ProcCPUInfo.java
+++ src/main/java/ise/ProcCPUInfo.java
@@ -1,20 +1,17 @@
-/**
- * Object to represent current state of /proc/cpuinfo virtual file when getProcCPUInfoTables() method is called
- * @author Mikey Fennelly
- * @version 2.0
- * */
-
 package ise;
 
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.util.ArrayList;
-import java.util.Hashtable;
-import java.util.List;
-import java.util.Arrays;
+import java.io.Reader;
+import java.util.*;
 import java.util.stream.Stream;
 
+/**
+ * Object to represent current state of /proc/cpuinfo virtual file when getProcCPUInfoTables() method is called
+ * @author Mikey Fennelly
+ * @version 2.0
+ */
 public class ProcCPUInfo  {
     private static final List<String> parseIntItemsList = Arrays.asList("clflush size", "cache alignment", "processor", "cpu family", "model", "stepping", "physical id", "siblings", "core id", "cpu cores", "apicid", "initial apicid", "cpuid level","cflush size", "cache_alignment");
     private static final List<String> parseBooleanFromYesNoItemsList = Arrays.asList("fpu", "fpu_exception", "wp");
@@ -34,8 +31,8 @@ public class ProcCPUInfo  {
         return addressSizes;
     });
 
-    public List<Hashtable<String, Object>> getProcCPUInfoTables () throws FileNotFoundException {
-        List<Hashtable<String, Object>> procCPUInfoTables = new ArrayList<Hashtable<String, Object>>();
+    public List<Map<String, Object>> getProcCPUInfoTables () throws FileNotFoundException {
+        List<Map<String, Object>> procCPUInfoTables = new ArrayList<Map<String, Object>>();
 
         KVPParser parser = new KVPParser();
         parser.addConversion(parseIntItemsList, KVPParser.premadeConversionOperation.PARSE_INT);
@@ -47,29 +44,26 @@ public class ProcCPUInfo  {
         parser.addConversion(Arrays.asList("address sizes"), processAddressSizes);
 
         // read file into a String[], each item representing a line
-        FileReader file = new FileReader("/proc/cpuinfo");
+        Reader file = new FileReader("/proc/cpuinfo");
         BufferedReader reader = new BufferedReader(file);
         Stream<String> linesStream = reader.lines();
         String[] lines = linesStream.toArray(String[]::new);
 
         // iterate through each line, and parse each line
-        Hashtable<String, Object> thisCPU = new Hashtable<String, Object>();
+        Map<String, Object> thisCPU = new Hashtable<String, Object>();
         for (String line : lines) {
-            String lineToOperateOn = line;
-            if (lineToOperateOn.trim().length() == 0) { // if a blank line is encountered, append current cpu and start the creation of a new one
-                procCPUInfoTables.add(thisCPU);
-            } else if (lineToOperateOn.contains("power management")) {
-                // Do nothing with this line (ingnore it)
-            } else { // otherwise, attempt to parse the line and add to the CPU
-                try {
-                    KVP thisLineKVP = parser.process(":", lineToOperateOn);
-                    thisCPU.put(thisLineKVP.getKey(), thisLineKVP.getValue());
-                } catch (NullPointerException e) {
-                    System.out.println();
-                    System.out.println(lineToOperateOn);
-                    e.printStackTrace();
-                }
+            if (line.trim().isEmpty()) {
+                continue;
             }
+            if (line.contains("power management")) {
+                // if a "power management" line is encountered, append current cpu and start the creation of a new one
+                procCPUInfoTables.add(thisCPU);
+                thisCPU = new Hashtable<String, Object>();
+                continue;
+            }
+
+            KVP thisLineKVP = parser.process(":", line);
+            thisCPU.put(thisLineKVP.getKey(), thisLineKVP.getValue());
         }
 
         return procCPUInfoTables;
