cd2352551ab51b8129d732354829406d811f7bc0
Mikey Fennelly <mikeyp.fennelly@gmail.com>
2024-10-27 19:36:45 +0000

    Process.processCpusAllowedList() added | Process.processMemsAllowed added

--- src/main/java/ise/Process.java
+++ src/main/java/ise/Process.java
@@ -1,16 +1,17 @@
 /**
  * Retrieves and represents information about a process such as memory (virtual and resident)
  * usage, CPU usage, process user, PID, PPID, priority, niceness.
- *
  * @author Mikey Fennelly
  * @version 1.0
  * */
 
 package ise;
 
-import java.io.IOException;
+import jdk.incubator.vector.VectorOperators;
+
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.IntStream;
 
 enum ProcState {
     RUN, SLEEP, UNINTERRUPTABLE_SLEEP, STOPPED, ZOMBIE
@@ -22,9 +23,21 @@ enum ProcState {
  * */
 public class Process {
     private final List<String> trimOnlyItemsList = Arrays.asList("Name", "Speculation_Store_Bypass", "SpeculationIndirectBranch", "Cpus_allowed");
-    private final List<String> popThreeCharsReturnIntList = Arrays.asList("VmPeak", "VmSize", "VmLck", "VmPin", "VmHWM", "VmRSS", "RssAnon", "RssFile", "RssShmem", "VmData", "VmStk", "VmExe", "VmLib", "VmPTE", "VmSwap", "HugetlbPages");
+    private final List<String> popThreeCharsReturnIntList = Arrays.asList("VmPeak", "VmSize", "VmLck", "VmPin", "VmHWM", "VmRSS", "RssAnon", "RssFile", "RssShmem", "VmData", "VmStk", "VmExe", "VmLib", "VmPTE", "VmSwap", "HugetlbPages", "untag_mask", "SigPnd", "ShdPnd", "SigBlk", "SigIgn", "SigCgt", "CapInh", "CapPrm", "CapEff", "CapBnd", "CapAmb");
+    private final List<String> parseLongList = Arrays.asList("Tgid", "Ngid", "Pid", "PPid", "TracerPid", "FDSize", "NStgid", "NSpid", "NSpgid", "NSsid", "Kthread", "CoreDumping", "THP_enabled", "Threads", "voluntary_ctxt_switches", "nonvoluntary_ctxt_switches");
 
-    private static ConversionOperation processProcState = (state -> {
+    private ConversionOperation processMemsAllowed = (memsAllowedString -> {
+        String[] memsAllowed = memsAllowedString.trim().split(",");
+        return memsAllowed;
+    });
+
+    private ConversionOperation processCpusAllowedList = (rangeString -> {
+        String[] splitRangeString = rangeString.trim().split("-");
+        int[] range = IntStream.rangeClosed(Integer.parseInt(splitRangeString[0]), Integer.parseInt(splitRangeString[1])).toArray();
+        return range;
+    });
+
+    private ConversionOperation processProcState = (state -> {
         ProcState stateToReturn = null;
         switch (state) {
             case "S (sleeping)":
