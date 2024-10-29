cafbcae064a07b6d9d8d36174f8b68fdc3ea7913
CiaranLy <lynchbros3@gmail.com>
2024-10-29 20:49:03 +0000

    updated cli now has cpu info

--- src/main/java/ise/CLI.java
+++ src/main/java/ise/CLI.java
@@ -6,13 +6,13 @@ import org.apache.commons.lang3.*;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.rmi.MarshalException;
 import java.sql.SQLOutput;
 import java.util.*;
 
 public class CLI {
     // set up the options
     private static final Option cpu = new Option("c","cpu",false,"display relevant cpu information");
-    private static final Option cpuSearch = new Option("core",false,"search for data for a specific core");
 
     private static final Option busses = new Option("b","busses",false,"display relevant bus information");
 
@@ -32,26 +32,9 @@ public class CLI {
         HelpFormatter formatter = new HelpFormatter();
         PrintWriter pw = new PrintWriter(System.out);
         pw.println();
-        if (options.hasOption(cpuSearch.getOpt())){
-            formatter.printUsage(pw,100, "java -jar RCHM.jar [options] [choice]");
-        }else {
-            formatter.printUsage(pw, 100, "java -jar RCHM.jar [options]");
-        }
+        formatter.printUsage(pw, 100, "java -jar RCHM.jar [options] [extra arguments]");
         formatter.printOptions(pw, 100, options, 2, 5);
         pw.close();
-    }
-
-    private static void resetOptions(Option... optionsToRetain) {
-        Set<Option> resetTo = new HashSet<>(Arrays.asList(optionsToRetain));
-
-        // Create a new Options object and add only the options in retainSet
-        Options retainedOptions = new Options();
-        for (Option option : resetTo) {
-            retainedOptions.addOption(option);
-        }
-
-        // Replace the original options with retainedOptions
-        CLI.options = retainedOptions;
     }
 
     // Method for the  display bar
@@ -100,47 +83,35 @@ public class CLI {
         System.out.printf("%s Buffers%n%d %s%n",displayBar(memInfo.buffers, memInfo.total),memInfo.buffers/ratio, unit);
 
     }
-  public static void cpuInfo(String choice) throws IOException {
-        ProcStatVF cpu = new ProcStatVF("/proc/stat");
+  public static void cpuInfo(String cpuNo) throws IOException {
+      // Create an instance of ProcCPUInfo
+      ProcCPUInfo cpuInfo = new ProcCPUInfo();
 
-        // Get CPU occupation tables in jiffies
-        Hashtable<String, Hashtable<String, Long>> cpuOccupationTables = cpu.getCPUOccupationTablesJiffies();
-        if (choice.isEmpty()) {
-            // Print the detailed CPU occupation times for each CPU
-            for (String cpuKey : cpuOccupationTables.keySet()) {
-                System.out.println("CPU: " + cpuKey);
-                Hashtable<String, Long> occupationTable = cpuOccupationTables.get(cpuKey);
-                for (String task : occupationTable.keySet()) {
-                    System.out.println("  " + task + ": " + occupationTable.get(task) + " jiffies");
-                }
-            }
-        }else{
-            boolean choicePresent = false;
-            for (String cpuKey : cpuOccupationTables.keySet()){
-                if (cpuKey.equals(choice)){
-                    System.out.println("CPU: " + cpuKey);
-                    Hashtable<String, Long> occupationTable = cpuOccupationTables.get(cpuKey);
-                    for (String task : occupationTable.keySet()) {
-                        System.out.println("  " + task + ": " + occupationTable.get(task) + " jiffies");
-                    }
-                    choicePresent = true;
-                }
-            }
-            if (!choicePresent){
-                printHelp(options);// if the entered string is not a cpuKey run help command
-            }
-        }
+      // Call getProcCPUInfoTables to retrieve the CPU information
+      List<Map<String, Object>> cpuInfoTables = cpuInfo.getProcCPUInfoTables();
+      System.out.println();
+      // Print the CPU information
+      System.out.println("CPU Information:");
+      boolean wrongcpu = true;
+      int cpuNumber = 1;
+      for (Map<String, Object> cpuTable : cpuInfoTables) {
+          if (cpuNo.equals("") || cpuNumber == Integer.parseInt(cpuNo.substring(3))){
+              System.out.println("CPU " + cpuNumber + ":");
+              wrongcpu = false;
+              for (Map.Entry<String, Object> entry : cpuTable.entrySet()) {
+                  System.out.printf("  %s: %s%n", entry.getKey(), entry.getValue());
+              }
+          }
+          cpuNumber++;
+          System.out.println();
+          if (wrongcpu){
+              System.out.println("That is not a valid cpu try a different one or use --help");
+          }
+      }
     }
 
     public static void processInfo(){
-        // Call the getAllRunningPIDs method from the SystemProcessInfo class
-        ArrayList<Integer> runningPIDs = SystemProcessInfo.getAllRunningPIDs();
 
-        // Print the retrieved process IDs
-        System.out.println("Currently running process IDs:");
-        for (Integer pid : runningPIDs) {
-            System.out.println(pid);
-        }
     }
 
     public static void main(String[] args){
@@ -166,30 +137,28 @@ public class CLI {
                 printHelp(options);
             }
             if (cl.hasOption(cpu.getLongOpt())) {
-                resetOptions(cpu, busses, memory, process, help);
-                options.addOption(cpuSearch);
-                cpuInfo("");// no input to give all cpu data
-            } else if (cl.hasOption(cpuSearch.getOpt())){
-                cpuInfo(cl.getArgList().get(0));// argument gives data on certain core
+                if (args.length == 1) {
+                    cpuInfo("");
+                } else {
+                    cpuInfo(args[1]);
+                }
             } else if (cl.hasOption(busses.getLongOpt())) {
-                resetOptions(cpu, busses, memory, process, help);
                 System.out.println("busses");
             } else if (cl.hasOption(memory.getLongOpt())) {
-                resetOptions(cpu, busses, memory, process, help);
-                options.addOption(kb);
-                options.addOption(mb);
-                options.addOption(gb);
-                memoryInfo("mb");
-            } else if (cl.hasOption(kb.getOpt())) {
-                memoryInfo("kb");
-            } else if (cl.hasOption(mb.getOpt())) {
-                memoryInfo("mb");
-            } else if (cl.hasOption(gb.getOpt())) {
-                memoryInfo("gb");
+                if (args[1] == null){
+                    memoryInfo("mb");
+                } else if (args[1].equals("kb")){
+                    memoryInfo("kb");
+                } else if (args[1].equals("mb")){
+                    memoryInfo("mb");
+                } else if (args[1].equals("gb")){
+                    memoryInfo("gb");
+                } else {
+                    printHelp(options);
+                }
             } else if (cl.hasOption(process.getLongOpt())){
-                resetOptions(cpu, busses, memory, process, help);
                 processInfo();
-            }else if (cl.hasOption(help.getLongOpt())) {
+            } else if (cl.hasOption(help.getLongOpt())) {
                 printHelp(options);
             } else {
                 printHelp(options);
