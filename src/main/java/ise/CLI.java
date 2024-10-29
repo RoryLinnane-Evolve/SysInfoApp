8d02c0738b1956d004d42ab09cc169f8a926ec4d
Mikey Fennelly <mikeyp.fennelly@gmail.com>
2024-10-29 21:10:35 +0000

    Implement Sysinfo for ProcCPUInfo

--- src/main/java/ise/CLI.java
+++ src/main/java/ise/CLI.java
@@ -12,41 +12,11 @@ import java.util.stream.Stream;
  * @version 1.0
  */
 public class CLI {
-    private static Map<Option, ArrayList<String>> optionsAndValidValues = new HashMap<Option, ArrayList<String>>();
-
-    static Option markMode = Option.builder("m")
-            .longOpt("markMode")
-            .desc("Submodule used to specify system information and attached devices")
-            .hasArgs()
-            .valueSeparator(' ')
-            .hasArg(true)
-            .build();
+    private static List<Sysinfo> sysInfos = Arrays.asList(
+      new ProcCPUInfo()
+    );
 
     public static void main(String[] programArgs) {
-        Options validOptions = new Options();
-        HelpFormatter helpFormatter = new HelpFormatter();
-        CommandLineParser parser = new DefaultParser();
-
-        validOptions.addOption(markMode);
-        try {
-            CommandLine cmd = parser.parse(validOptions, programArgs);
-            processOptions(cmd);
-        } catch (ParseException e) {
-            System.err.println("Parsing failed. Reason: " + e.getMessage());
-        }
-    }
-
-    private static void listPCIDevices() {
-        System.out.println("PCI Devices");
-    }
-
-    private static void processOptions(CommandLine cmd) throws ParseException {
-        // iterate through all options parser parsed
-        for (Option option : cmd.getOptions()) {
-            String[] optionVals = option.getValues();
-        }
-        // iterate through options
-        // check if the option values are valid
-        // check validity of optionValues for the arg
+        sysInfos.forEach(Sysinfo::printToConsole);
     }
 }
