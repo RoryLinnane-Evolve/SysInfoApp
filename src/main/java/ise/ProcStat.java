b2ab138e44864a633101d5247f8caa3419c5bc26
Mikey Fennelly <mikeyp.fennelly@gmail.com>
2024-10-29 14:56:41 +0000

    parseProcPIDStat working
    Parsing to BigInteger for convenience

--- src/main/java/ise/ProcStat.java
+++ src/main/java/ise/ProcStat.java
@@ -1,16 +1,20 @@
 package ise;
 
 import java.io.IOException;
+import java.math.BigInteger;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 class ProcStat {
     public static void main(String[] args) throws IOException {
-        parseProcPIDStat(3157);
+        Map<String, Object> procPidStat = parseProcPIDStat(3157);
+
+        int d = 2;
     }
 
-    private static void parseProcPIDStat(int pid) throws IOException {
+    private static Map<String, Object> parseProcPIDStat(int pid) throws IOException {
         KVPParser parser = new KVPParser();
         String[] lines = parser.getLines("/proc/" + pid + "/stat" );
         String onlyLineInFile = lines[0];
@@ -18,8 +22,26 @@ class ProcStat {
 
         List<String> onlyLineInFileKeys = Arrays.asList("pid", "comm", "state", "ppid", "pgrp", "session", "tty_nr", "tpgid", "flags", "minflt", "cminflt", "majflt", "cmajflt", "utime", "stime", "cutime", "cstime", "priority", "nice", "num_threads", "itrealvalue", "starttime", "vsize", "rss", "rsslim", "startcode", "endcode", "startstack", "kstkesp", "kstkeip", "signal", "blocked", "sigignore", "sigcatch", "wchan", "nswap", "cnswap", "exit_signal", "processor", "rt_priority", "policy", "delayacct_blkio_ticks", "guest_time", "cguest_time", "start_data", "end_data", "start_brk", "arg_start", "arg_end", "env_start", "env_end", "exit_code");
 
+        Map<String, Object> returnMap = new HashMap<>();
+
         for (int i = 0; i < onlyLineInFileKeys.size(); i++) {
-            System.out.println(onlyLineInFileKeys.get(i) + " : " + onlyLineInFileValsSplit[i]);
+            String thisLineKey = onlyLineInFileKeys.get(i);
+            String thisLineValueString = onlyLineInFileValsSplit[i];
+            System.out.println(thisLineKey + " : " + thisLineValueString);
+
+            try {
+                if (thisLineKey == "comm" || thisLineKey == "state") {
+                    String thisLineVal = thisLineValueString;
+                    returnMap.put(thisLineKey, thisLineVal);
+                } else {
+                    BigInteger thisLineVal = new BigInteger(thisLineValueString);
+                    returnMap.put(thisLineKey, thisLineVal);
+                }
+            } catch (Exception e) {
+                System.err.println(e.getMessage());
+            }
         }
+
+        return returnMap;
     }
 }
