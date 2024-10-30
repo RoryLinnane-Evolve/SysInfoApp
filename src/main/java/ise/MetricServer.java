003d7e0d36a006aa7ec35038e6bed38113d1e7ea
Rory Linnane <rorylinnane007@gmail.com>
2024-10-30 20:03:13 +0000

    Dashboard updated. Extra Process info added

--- src/main/java/ise/MetricServer.java
+++ src/main/java/ise/MetricServer.java
@@ -1,11 +1,11 @@
 /**
- * This is the file for the server that recieves get requests
- * from prometheus and responds with the data in the gauges set out below.
- * The data is set every 4 seconds. There is 2 threads running simultaneously
+ * This is the file for the server that receives get requests
+ * from Prometheus and responds with the data in the gauges set out below.
+ * The data is set every 4 seconds. There are 2 threads running simultaneously.
  *
  * @author Rory Linnane
  * @version 1.0
- * */
+ */
 
 package ise;
 
@@ -20,14 +20,10 @@ import java.io.IOException;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.util.ArrayList;
-import java.util.Hashtable;
 import java.util.List;
 import java.util.Map;
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 public class MetricServer {
 
@@ -54,62 +50,60 @@ public class MetricServer {
 
     final Gauge systemCPUUsage = Gauge.build()
             .name("system_CPU_usage")
-            .help("The percentage cpu usage")
+            .help("The percentage CPU usage")
             .register();
 
     final Gauge processInfo = Gauge.build()
             .name("system_process_info")
             .help("Information about system processes")
-            .labelNames("pid", "name", "state")
+            .labelNames("pid", "name", "state", "comm", "priority", "vsize", "stime", "kstkesp", "kstKeip", "majflt", "end_data", "utime", "rsslim", "ppid")
             .register();
 
     final Gauge pciInfo = Gauge.build()
             .name("system_pci_info")
-            .help("contains all the information about the pci buses in the system.")
+            .help("Contains all the information about the PCI buses in the system.")
             .labelNames("busCount", "deviceCount", "functionCount", "functionsPresent")
             .register();
 
     final Gauge usbInfo = Gauge.build()
             .name("system_usb_info")
-            .help("all system usb information")
+            .help("All system USB information")
             .labelNames("busCount", "deviceCount", "vendorID", "productID")
             .register();
 
     final Gauge diskInfo = Gauge.build()
             .name("system_disk_info")
-            .help("all system disk information")
+            .help("All system disk information")
             .labelNames("diskCount", "deviceName", "totalDisks", "usedDisks", "availableDisks")
             .register();
 
-    private String IPAddress ;
+    private String IPAddress;
     private int port;
 
-    public MetricServer(String _IPAddress, int _port){
+    public MetricServer(String _IPAddress, int _port) {
         IPAddress = _IPAddress;
         port = _port;
     }
 
-
-    public void start() throws IOException{
-        // Starts a http server with the specified port and IP
-        // metrics are exposed at <localIp>:port/metrics
+    public void start() throws IOException {
+        // Starts an HTTP server with the specified port and IP
+        // Metrics are exposed at <localIp>:port/metrics
         InetSocketAddress address = new InetSocketAddress(IPAddress, port);
         HTTPServer server = new HTTPServer.Builder().withInetSocketAddress(address).build();
 
-
-        //gets the pci info and updates the gauge with the values
+        // Gets the PCI info and updates the gauge with the values
         pciInfo.labels(String.valueOf(PCIInfo.getBusCount()),
                 String.valueOf(PCIInfo.getDeviceCount()),
                 String.valueOf(PCIInfo.getFunctionCount()),
                 String.valueOf(PCIInfo.getFunctionPresent()));
 
-        //gets the usb info and updates the gauge with the values
+        // Gets the USB info and updates the gauge with the values
         usbInfo.labels(String.valueOf(USBInfo.getBusCount()),
                 String.valueOf(USBInfo.getDeviceCount()),
                 String.valueOf(USBInfo.getVendorID()),
                 String.valueOf(USBInfo.getProductID()));
 
-        //gets the disk information and updates gauges with the values
+        // Gets the disk information and updates gauges with the values
         String[] diskInformationArray = DiskInfo.getAllDiskInformation();
         diskInfo.labels(diskInformationArray[0],
                 diskInformationArray[1],
@@ -117,16 +111,16 @@ public class MetricServer {
                 diskInformationArray[3],
                 diskInformationArray[4]);
 
-        //creating the thread for the memory and cpu metrics to be updated
+        // Creating the thread for the memory and CPU metrics to be updated
         Thread memAndCPU = new Thread(() -> {
             try {
-                systemMemmoryAndCPUUpdater();
+                systemMemoryAndCPUUpdater();
             } catch (Exception e) {
                 throw new RuntimeException(e);
             }
         });
 
-        //creating the thread for the process metrics to be updated
+        // Creating the thread for the process metrics to be updated
         Thread procs = new Thread(() -> {
             try {
                 processesUpdater();
@@ -135,45 +129,38 @@ public class MetricServer {
             }
         });
 
-        //starting the threads
+        // Starting the threads
         procs.start();
         memAndCPU.start();
     }
 
-    private void systemMemmoryAndCPUUpdater() throws Exception{
-
-        //creating the object for cpu information
+    private void systemMemoryAndCPUUpdater() throws Exception {
+        // Creating the object for CPU information
         VirtualFileInfo cpuinfo = new VirtualFileInfo("/proc/cpuinfo");
 
-        while(true){
-            try{
+        while (true) {
+            try {
                 // Get system memory information
                 SystemMemoryInfo mem = new SystemMemoryInfo();
 
-                // Update the memory Gauges with the current memory info
+                // Update the memory gauges with the current memory info
                 totalMemory.set(mem.total);
                 freeMemory.set(mem.free);
 
-                // get the memory usage percentage by dividing the (total memory - free memory) by the total memory and multiplying by 100
-                double memusage = (double)(mem.total-mem.free)/(mem.total) * 100;
-                systemMemoryUsage.set(Math.round(memusage));
+                // Get the memory usage percentage by dividing the (total memory - free memory) by the total memory and multiplying by 100
+                double memUsage = (double) (mem.total - mem.free) / mem.total * 100;
+                systemMemoryUsage.set(Math.round(memUsage));
 
-
-
-
-
-                //Update cpu mhz in the hashtable
+                // Update CPU MHz in the hashtable
                 cpuinfo.setHashtable();
-                //retrieve it from Hashtable and update gauge
+                // Retrieve it from Hashtable and update gauge
                 double cpuMHz = Double.parseDouble(cpuinfo.fileInfo.get("cpu MHz"));
                 systemCPUMHz.set(cpuMHz);
 
-                //TODO: Calculate CPU Usage
+                // TODO: Calculate CPU Usage
                 List<Long> prevStats = getCPUStats();
-                //Delay for 4 seconds
+                // Delay for 4 seconds
                 Thread.sleep(4000);
-
-
 
                 List<Long> currStats = getCPUStats();
 
@@ -182,39 +169,49 @@ public class MetricServer {
                 systemCPUUsage.set(cpuUsage);
 
             } catch (Exception e) {
-                e.printStackTrace();
+                throw e;
             }
         }
     }
 
-    private void processesUpdater() throws Exception{
-
+    private void processesUpdater() throws Exception {
         ProcPIDStat procStat = new ProcPIDStat();
         ProcPIDStatus procStatus = new ProcPIDStatus();
-        while (true){
-            try{
-                //Get process
+
+        while (true) {
+            try {
+                // Get process
                 ArrayList<Integer> pids = SystemProcessInfo.getAllRunningPIDs();
 
-                //Loop through the process ids
+                // Loop through the process IDs
                 for (int id : pids) {
-                    //gets the process infromation for the current id
-                    Map<String, Object> procStatusInfo =  procStatus.getProcessInfo(id);
-
+                    // Gets the process information for the current ID
+                    Map<String, Object> procStatusInfo = procStatus.getProcessInfo(id);
                     Map<String, Object> procStatInfo = procStat.getProcPIDStatInfo(id);
-                    
-                    //updates the gauge list for the current process
+
+                    // Updates the gauge list for the current process
                     processInfo.labels(
                             String.valueOf(procStatusInfo.get("Pid")),
                             String.valueOf(procStatusInfo.get("Name")),
-                            String.valueOf(procStatusInfo.get("State"))
+                            String.valueOf(procStatusInfo.get("State")),
+                            (String) procStatInfo.get("comm"),
+                            String.valueOf(procStatInfo.get("priority")),
+                            String.valueOf(procStatInfo.get("vsize")),
+                            String.valueOf(procStatInfo.get("stime")),
+                            String.valueOf(procStatInfo.get("kstkesp")),
+                            String.valueOf(procStatInfo.get("kstkeip")),
+                            String.valueOf(procStatInfo.get("majflt")),
+                            String.valueOf(procStatInfo.get("end_data")),
+                            String.valueOf(procStatInfo.get("utime")),
+                            String.valueOf(procStatInfo.get("rsslim")),
+                            String.valueOf(procStatInfo.get("ppid"))
                     ).set(0);
                 }
 
-                //Delay 4 seconds
+                // Delay 4 seconds
                 Thread.sleep(4000);
-            } catch (Exception e){
-                e.printStackTrace();
+            } catch (Exception e) {
+                throw e;
             }
         }
     }
@@ -232,6 +229,7 @@ public class MetricServer {
         }
         return stats;
     }
+
     private static double calculateCPUUsage(List<Long> prevStats, List<Long> currStats) {
         long prevIdle = prevStats.get(3) + prevStats.get(4);
         long currIdle = currStats.get(3) + currStats.get(4);
